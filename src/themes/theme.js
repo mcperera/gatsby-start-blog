b650e3bce90bfc1f1fbbe4bd75711d371723120f
Madushan Perera <madushanperera3817@gmail.com>
2021-01-08 16:30:34 +0530

    Add styled component with themes

---
+++ src/themes/theme.js
@@ -0,0 +1,35 @@
+export default {
+  fonts: {
+    main: "Muli, sans-serif",
+    code: "Robotp Mono, monospace",
+  },
+  colors: {
+    main1: "hsl(207,70%,60%)",
+    main2: "hsl(207,70%,95%)",
+    dark1: "hsl(207,2%,12%)",
+    dark2: "hsl(207,2%,26%)",
+    dark3: "hsl(207,2%,64%)",
+    light1: "hsl(0,0%,97%)",
+    light2: "hsl(0,0%,99%)",
+  },
+  berakpoints: {
+    mobile: "only screen and (max-width: 50rem)",
+    desktop: "only screen and (max-width: 65rem)",
+  },
+  spacings: {
+    xxSmall: ".25rem",
+    xSmall: "0.5rem",
+    small: "1rem",
+    medium: "2rem",
+    large: "3rem",
+    xLarge: "4rem",
+    xxLarge: "5rem",
+  },
+  animation: {
+    button: "box-shadow 0.3s ease",
+    link: "color 0.2s ease",
+  },
+  shadows: {
+    shadow1: "0px 5px 20px rgba(30, 30, 31, 0.05)",
+  },
+}
