f2bff022d776bca5e3ffec35c0cf36e1fe954a26
Madushan Perera <madushanperera3817@gmail.com>
2021-01-09 18:01:40 +0530

    Add nav with logo

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,7 +1,13 @@
 import React from "react"
 
 import { ContainerWrapper } from "../elements"
+import { Nav } from "../components"
 
 export const Container = ({ children }) => {
-  return <ContainerWrapper>{children}</ContainerWrapper>
+  return (
+    <ContainerWrapper>
+      <Nav />
+      {children}
+    </ContainerWrapper>
+  )
 }
