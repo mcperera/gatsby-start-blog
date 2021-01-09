f2ac19764ff6d1f73d4e664f33efda7de438e755
Madushan Perera <madushanperera3817@gmail.com>
2021-01-09 20:34:31 +0530

    Add content wrapper

---
+++ src/components/Content.js
@@ -0,0 +1,6 @@
+import React from "react"
+import { ContentWrapper } from "../elements/ContentElement"
+
+export const Content = ({ children }) => {
+  return <ContentWrapper>{children}</ContentWrapper>
+}
