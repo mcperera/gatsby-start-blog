02ece2b680d93dc2e0824aeb333cf31f6f6f0f87
Madushan Perera <madushanperera3817@gmail.com>
2021-01-10 20:03:01 +0530

    Create card component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,18 @@
 import React from "react"
-import { Container, FeatureImage } from "./../components"
+import { Container, FeatureImage, Content, ContentCard } from "./../components"
 
 const IndexPage = () => {
   return (
     <Container>
       <FeatureImage />
+      <Content>
+        <ContentCard
+          date="2021/1/10"
+          title="Test"
+          excerpt="Lorem ipsum, dolor sit amet consectetur adipisicing elit."
+          slug="https://www.google.com"
+        />
+      </Content>
     </Container>
   )
 }
