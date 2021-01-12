539541ed1a27c1f0d26dae2346cffc14fc95f631
Madushan Perera <madushanperera3817@gmail.com>
2021-01-12 16:50:00 +0530

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,7 +27,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `pages`,
-        path: `${__dirname}/src/pages`,
+        path: `${__dirname}/opt/build/repo/src/pages`,
       },
     },
     {
