7b9e1015f6395c844cf1e922262fa607060f4895
Madushan Perera <madushanperera3817@gmail.com>
2021-01-12 17:09:08 +0530

    fix netlify build

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import React from "react"
+import { Container, FeatureImage, Content, ContentCard } from "./../components"
+
+const IndexPage = () => {
+  return <Container></Container>
+}
+
+export default IndexPage
